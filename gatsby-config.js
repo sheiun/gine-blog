6589ea25102775633eaa796174a94f5d20196f28
雪煾 <me.sheiun@gmail.com>
2020-04-19 06:02:01 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
+                configTable: "https://www.notion.so/2818e2353e8f44d282bc34cb00fd4eea"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
+                configTable: "https://www.notion.so/6b1628ab5d544065bf53b13c85dc4be2"
             }
         }
     ],
